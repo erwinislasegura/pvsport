d6b19050a4fadfcf35ba214350fcd1ca22d24b7e
desarrollovolcan <eisla@fvolcan.cl>
2026-04-19 17:01:40 -0400

    BD

--- configuracion/base_datos.php
+++ configuracion/base_datos.php
@@ -3,9 +3,9 @@
 return [
     'host' => $_ENV['DB_HOST'] ?? 'localhost',
     'puerto' => $_ENV['DB_PUERTO'] ?? '3306',
-    'nombre' => $_ENV['DB_NOMBRE'] ?? 'pvsport',
-    'usuario' => $_ENV['DB_USUARIO'] ?? 'root',
-    'password' => $_ENV['DB_PASSWORD'] ?? '',
+    'nombre' => $_ENV['DB_NOMBRE'] ?? 'gocreative_web_pvsport',
+    'usuario' => $_ENV['DB_USUARIO'] ?? 'gocreative_eisla_web',
+    'password' => $_ENV['DB_PASSWORD'] ?? 'Eisla1245.$',
     'charset' => $_ENV['DB_CHARSET'] ?? 'utf8mb4',
     'socket' => $_ENV['DB_SOCKET'] ?? '',
 ];
